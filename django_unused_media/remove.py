2f15c272a8964835f2a4d2739ebc8c2ac38b2c29
Francis Secada <francis.secada@gmail.com>
2019-12-18 12:07:23 -0500

    Added get_path function to check MEDIA_URL for validation; default to MEDIA_ROOT if not. Should enable cloud functionality.

--- django_unused_media/remove.py
+++ django_unused_media/remove.py
@@ -3,6 +3,22 @@
 import os
 
 from django.conf import settings
+from django.core.validators import URLValidator
+
+
+def get_path():
+    '''A simple python script to validate MEDIA_URL. If var is local,
+    validation will fail and will default to MEDIA_ROOT. Else, var will be
+    set to MEDIA_URL.
+    This should make the script compliant with cloud storage instances.
+    '''
+    path = settings.MEDIA_URL
+    val = URLValidator()
+    try:
+        val(path)
+    except ValidationError:
+        path = settings.MEDIA_ROOT
+    return path
 
 
 def remove_media(files):
@@ -10,7 +26,7 @@ def remove_media(files):
         Delete file from media dir
     """
     for filename in files:
-        os.remove(os.path.join(settings.MEDIA_ROOT, filename))
+        os.remove(os.path.join(get_path(), filename))
 
 
 def remove_empty_dirs(path=None):
@@ -19,7 +35,7 @@ def remove_empty_dirs(path=None):
     """
 
     if not path:
-        path = settings.MEDIA_ROOT
+        path = get_path()
 
     if not os.path.isdir(path):
         return False
